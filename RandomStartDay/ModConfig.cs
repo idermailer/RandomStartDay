45ccee89bcb8535a412e96886d301a6a221eb163
IN <IN@DESKTOP-94LCCCD>
2022-08-02 21:29:31 +0900

    초기 커밋.

---
+++ RandomStartDay/ModConfig.cs
@@ -0,0 +1,13 @@
+﻿class ModConfig
+{
+    public bool isRandomSeedUsed { get; set; }
+    public string[] allowedSeasons { get; set;}
+    public int MaxOfDayOfMonth { get; set;}
+
+    public ModConfig()
+    {
+        this.isRandomSeedUsed = true;
+        this.allowedSeasons = new string[] {"spring", "summer", "fall", "winter"};
+        this.MaxOfDayOfMonth = 28;
+    }
+}
